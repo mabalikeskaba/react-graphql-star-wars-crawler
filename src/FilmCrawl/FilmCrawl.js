e75fa1f45eee8cc8250df96dc79dccf42ad2b9bd
Manuel Bauer <maba0809@gmail.com>
2020-04-29 10:59:27 +0200

    modified some styles

--- src/FilmCrawl/FilmCrawl.js
+++ src/FilmCrawl/FilmCrawl.js
@@ -9,9 +9,35 @@ const getFilmById = gql`
       title
       openingCrawl
       id
+      episodeId
     }
   }
 `;
+
+function getEpisodeString(episodeId) {
+  switch (episodeId) {
+    case 1:
+      return 'I';
+    case 2:
+      return 'II';
+    case 3:
+      return 'III';
+    case 4:
+      return 'IV';
+    case 5:
+      return 'V';
+    case 6:
+      return 'VI';
+    case 7:
+      return 'VII';
+    case 8:
+      return 'VIII';
+    case 9:
+      return 'IX';
+    default:
+      return episodeId;
+  }
+}
 
 const FilmCrawl = ({ id }) => {
   console.log('ID: ' + id);
@@ -25,10 +51,21 @@ const FilmCrawl = ({ id }) => {
   if (filmLoading) return <span>Loading...</span>;
   if (filmError) return <span>Error!...</span>;
   return (
-    <div className='filmCrawl'>
-      <div className='title'>{filmData.Film.title}</div>
-      <div className='crawlerText'>{filmData.Film.openingCrawl}</div>
-    </div>
+    <>
+      <div className='crawlContainer'>
+        <div className='leftBorder'></div>
+        <div className='filmCrawl'>
+          <div className='filmCrawlContent'>
+            <div className='filmCrawlTitle'>
+              <p>Episode {getEpisodeString(filmData.Film.episodeId)}</p>
+              <p>{filmData.Film.title}</p>
+            </div>
+            <div className='filmCrawlText'>{filmData.Film.openingCrawl}</div>
+          </div>
+        </div>
+        <div className='rightBorder'></div>
+      </div>
+    </>
   );
 };
 
